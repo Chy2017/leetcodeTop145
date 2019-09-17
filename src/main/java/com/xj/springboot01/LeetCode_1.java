5a7c622fb663d2a37745b466bc3d057348b01505
xiongjj <xiongjjjjj@outlook.com>
2019-09-17 19:21:35 +0800

    add leetcode04

--- src/main/java/com/xj/springboot01/LeetCode_1.java
+++ src/main/java/com/xj/springboot01/LeetCode_1.java
@@ -14,11 +14,10 @@ import java.util.HashMap;
         那我们遍历nums的时候，nums数组元素进入hashmap之前应该检查是否有（target - nums数组元素）已经在库中
         若有，则输出这两个 nums数组元素 对应的value（即题目要求的下标）
         若没有，则按照（nums数组元素所对应的值为key，nums数组元素的下标为value）入库
-*/
+ */
 public class LeetCode_1 {
 
     public static int[] twoSum(int[] nums, int target){
-
         HashMap<Integer,Integer> m = new HashMap<>();
         int ans[] = {-1, -1};
         for (int i = 0; i < nums.length; i++){
